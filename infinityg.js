c946f3a6c228aeb1b77391f25e9df2f4656ddf8a
airdropbomb <saginaw135@gmail.com>
2025-02-22 15:28:41 +0700

    Update infinityg.js

--- infinityg.js
+++ infinityg.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 const axios = require('axios');
-import cfonts from "cfonts";
-import chalk from 'chalk';
 
 async function main(numberOfInteractions) {
   cfonts.say('ADB Node', {
